2956149321a3517f16abbdfe8607271967433f19
Caua Mattos <caua.h4k@protonmail.com>
2025-05-07 19:23:09 -0300

    feat: starting to implement the File domain

--- csv-xlsx-filetracker-api/routes/web.php
+++ csv-xlsx-filetracker-api/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/api/files/upload', [FileController::class, 'store']);
