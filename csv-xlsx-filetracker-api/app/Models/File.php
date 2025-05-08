bc969f41571cfc07b433ac67ef1e4e7f65bbba10
Caua Mattos <cauamattos.dev@gmail.com>
2025-05-08 20:16:16 -0300

    feat(mongodb): add functional connection with MongoDB service in Docker and update File model to proper work with MongoDB

--- csv-xlsx-filetracker-api/app/Models/File.php
+++ csv-xlsx-filetracker-api/app/Models/File.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    protected $connection = 'mongodb';
+    protected $fillable = [];
 }
--- csv-xlsx-filetracker-api/bootstrap/providers.php
+++ csv-xlsx-filetracker-api/bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    MongoDB\Laravel\MongoDBServiceProvider::class,
 ];
