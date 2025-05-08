dbfd438cf945ef64fba9f0e5fede5bd07a94237a
Caua Mattos <cauamattos.dev@gmail.com>
2025-05-07 21:51:12 -0300

    feat: implementing /api/files/upload endpoint feat: implementing file upload function in FileController, with pagination and JSON response. chore: add RouteServiceProvider chore: php artisan install:api runned and added sanctum dependecy, use HasApiToken trait in User model, created PAT database migration

--- csv-xlsx-filetracker-api/app/Http/Controllers/FileController.php
+++ csv-xlsx-filetracker-api/app/Http/Controllers/FileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use SebastianBergmann\CodeCoverage\Report\Xml\Totals;
 
 class FileController extends Controller
 {
@@ -13,6 +15,93 @@ class FileController extends Controller
     public function index()
     {
         //
+    }
+
+    /**
+     * Function for uploading an object
+     */
+    public function upload(Request $request)
+    {
+        // $request->validate([
+        //     'file' => 'required|file|mimes:csv,xlsx|max:102400'
+        // ]);
+
+        $file = $request->file('file');
+
+        if ($file) {
+            $path = $file->getRealPath();
+            $csvFile = fopen($path, 'r');
+
+            $data = [];
+
+            /**
+             *  fgetcsv is called here to move the internal pointer to the next line and
+             *  desconsider the first line of the csv with 'Status do Arquivo: Final'
+             *  content
+             * */
+            fgetcsv($csvFile, null, ';');
+
+            $header = fgetcsv($csvFile, null, ';');
+
+            $row = fgetcsv($csvFile, null, ';');
+
+            $currentRow = 0;
+            $page = $request->input('page', 1);
+            $perPage = 10;
+            // #TODO add $page value
+            $offset = ($page - 1) * $perPage;
+
+
+            while ($row !== false) {
+                if ($currentRow >= $offset && $currentRow < $offset + $perPage) {
+                    $data[] = array_combine($header, $row);
+                }
+
+                $currentRow++;
+
+                if ($currentRow >= $offset + $perPage) {
+                    break;
+                }
+            }
+
+            fclose($csvFile);
+
+
+            $totalLines = $this->countCsvLines($path);
+            $dataLines = $totalLines - 2;
+
+            $itemsPerPage = (int) ($request->get('per_page', $perPage));
+            $totalPages = ceil($dataLines / $itemsPerPage);
+
+
+            return response()->json([
+                'message' => 'File uploaded successfully',
+                'data' => $data,
+                'page' => $page,
+                'perPage' => $itemsPerPage,
+                'totalPages' => $totalPages,
+            ]);
+        }
+
+        return response()->json(['error' => 'No file uploaded'], 400);
+    }
+
+    private function countCsvLines($path)
+    {
+        $lineCount = 0;
+        $handle = fopen($path, 'rb');
+
+        if ($handle === false) {
+            return 0;
+        }
+
+        while (!feof($handle)) {
+            $line = fread($handle, 8192);
+            $lineCount += substr_count($line, "\n");
+        }
+
+        fclose($handle);
+        return $lineCount;
     }
 
     /**
---
+++ csv-xlsx-filetracker-api/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\FileController;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/files/upload', [FileController::class, 'upload']);
