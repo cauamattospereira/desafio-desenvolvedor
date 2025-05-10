3e4883c44eed93cd39761c9d9071c2de884a2459
Caua Mattos <cauamattos.dev@gmail.com>
2025-05-10 16:53:19 -0300

    feat(File model): add hasUuids and SoftDeletes traits. Make all fields fillable with guarded empty array

--- csv-xlsx-filetracker-api/app/Models/File.php
+++ csv-xlsx-filetracker-api/app/Models/File.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\SoftDeletes;
 
 class File extends Model
 {
+    use HasUuids;
+    use SoftDeletes;
+
+    protected $table = 'filetracker-api-service';
     protected $connection = 'mongodb';
-    protected $fillable = [];
+    protected $guarded = [];
 }
