636e009a9e805ace14ceb56fa05f0b98b35a96cc
Caua Mattos <cauamattos.dev@gmail.com>
2025-05-11 19:01:24 -0300

    feat(file upload route): Finished file upload route.
    - prevent the user from uploading the same file twice
    - add a microtime function to calculate the amount of time is took to receive and save the file on MongoDB
    - add relevant information for the document on MongoDB, with all necessary fields to turn able to make simple queries on db
    - add a count for unvalid lines
    - add comments

--- csv-xlsx-filetracker-api/app/Http/Controllers/FileController.php
+++ csv-xlsx-filetracker-api/app/Http/Controllers/FileController.php
@@ -18,93 +18,157 @@ class FileController extends Controller
 
     /**
      * Function for uploading a CSV/Excel file
+     * 
+     * This functions separate the file in chunks of 10000 lines for saving
+     * on MongoDB without surpassing the limit of 16mb per document.
      */
     public function upload(Request $request)
     {
-        if ($request->hasFile('file') == false) {
-            return response()->json(
-                [
-                    'error' => "No valid file is found with 'file' key",
-                    'message' => 'Please, upload a valid CSV or Excel file in a form data'
-                ],
-                400
-            );
+        $start = microtime(true); // will serve for calculating the time it took for complete the process
+
+        if (!$request->hasFile('file')) {
+            return response()->json([
+                'error' => "No valid file is found with 'file' key",
+                'message' => 'Please, upload a valid CSV or Excel file in a form data',
+                'success' => false,
+            ], 400);
         }
 
         $file = $request->file('file');
+        $contentHash = md5_file($file->getRealPath());
+
+        $duplicate = File::where('content_hash', $contentHash)->first();
+        if ($duplicate) {
+            $duplicate->makeHidden(['data']);
+
+            return response()->json([
+                'error' => 'Duplicated file',
+                'message' => 'A file with this content already exists in the database.',
+                'duplicated_file' => $duplicate,
+                'success' => false,
+            ], 409);
+        }
+
+
+
+        // if (File::find($file->getClientOriginalName())) {
+        //     return ...
+        // }
 
         $path = $file->getRealPath();
         $csvFile = fopen($path, 'r');
 
-        $currentRow = 0;
-        $page = $request->input('page', 1);
-        $perPage = 100;
-        $offset = ($page - 1) * $perPage;
+        fgetcsv($csvFile, null, ';'); // Skip first line of the file (the file for test have a 'Status do arquivo' header)
+        $header = fgetcsv($csvFile, null, ';'); // Read real header
+
+        $linesPerChunk = 10000;
+        $chunkIndex = 1;
+        $rowCount = 0;
         $data = [];
+        $invalidLines = 0;
+        $totalProcessingTimeMs = 0;
+        $totalInvalidLines = 0;
 
-        /**
-         *  fgetcsv is called here to move the internal pointer to the next line and
-         *  desconsider the first line of the csv with 'Status do Arquivo: Final'
-         *  content
-         * */
-        fgetcsv($csvFile, null, ';');
-
-        /**
-         * get header and row content of the current line of the csv/excel file
-         */
-        $header = fgetcsv($csvFile, null, ';');
-        $row = fgetcsv($csvFile, null, ';');
-
-        /**
-         * feed the $data variable with the content of the csv/excel file
-         */
-        while ($currentRow < 10000) {
-            $data[] = array_combine($header, $row);
-            $currentRow++;
-        }
-
-        /**
-         * upload metadata
-         */
         $documentOriginalName = $file->getClientOriginalName();
-        $newFileName = date('Y-h-d_His') . '_' . $file->getClientOriginalName();
         $documentOriginalExtension = $file->getClientOriginalExtension();
-        $uploadedAt = Carbon::now();
+        $uploadedAtUtc = Carbon::now();
+        $uploadedAtBrasilia = Carbon::now('America/Sao_Paulo')->toIso8601String();
         $fileSize = $file->getSize();
         $totalLines = $this->countCsvLines($path);
-        $dataLines = $totalLines - 2;
-        $itemsPerPage = (int) ($request->get('per_page', $perPage));
-        $totalPages = ceil($dataLines / $itemsPerPage);
 
-        $storedFile = [
-            'uploaded_metadata' => [
+        while (($row = fgetcsv($csvFile, null, ';')) !== false) {
+            if (array_combine($header, $row) === false) {
+                $invalidLines += 1;
+            }
+
+            $data[] = array_combine($header, $row);
+
+            $rowCount++;
+
+            if ($rowCount % $linesPerChunk === 0) {
+                /**
+                 * logic for calculation the time it took for process the file
+                 */
+                $totalInvalidLines = $invalidLines;
+
+                $fileName = date('Y-m-d_His') . "_{$chunkIndex}_" . $documentOriginalName;
+
+                File::create([
+                    'filename' => $fileName,
+                    'original_filename' => $documentOriginalName,
+                    'content_hash' => $contentHash,
+                    'upload_date_brasilia_local_time' => $uploadedAtBrasilia,
+                    'uploaded_metadata' => [
+                        'original_file_size' => $fileSize,
+                        'original_extension' => $documentOriginalExtension,
+                    ],
+                    'processing_info' => [
+                        'chunk_total_lines' => $totalLines,
+                        'valid_lines' => count($data),
+                        'invalid_lines' => $invalidLines,
+                    ],
+                    'data' => $data,
+                ]);
+
+                // prepare for next iteration
+                $chunkIndex++;
+                $invalidLines = 0;
+                $data = [];
+            }
+        }
+
+        // save remaining data not included in other chunks
+        if (!empty($data)) {
+            $fileName = date('Y-m-d_His') . "_{$chunkIndex}_" . $documentOriginalName;
+
+            File::create([
+                'filename' => $fileName,
                 "original_filename" => $documentOriginalName,
-                "stored_as" => $newFileName,
-                "uploaded_at" => $uploadedAt,
-                "file_size" => $fileSize,
-                "extension" => $documentOriginalExtension
-            ],
-            'processing_info' => [
-                'total_lines' => $totalLines,
-                'valid_lines' => $dataLines,
-                'invalid_lines' => '0 [FAKE]',
-                'processing_time_ms' => '1532 [FAKE]'
-            ],
-            'data' => $data,
-        ];
-
+                'upload_date_brasilia_local_time' => $uploadedAtBrasilia,
+                'content_hash' => $contentHash,
+                'uploaded_metadata' => [
+                    "original_file_size" => $fileSize,
+                    "original_extension" => $documentOriginalExtension,
+                ],
+                'processing_info' => [
+                    'chunk_total_lines' => $totalLines,
+                    'valid_lines' => count($data),
+                    'invalid_lines' => $invalidLines,
+                ],
+                'data' => $data,
+            ]);
+        }
 
         fclose($csvFile);
 
-        File::create($storedFile);
+        $end = microtime(true);
+        $processingTimeMs = round(($end - $start) * 1000);
+        $totalProcessingTimeMs += $processingTimeMs;
 
         return response()->json([
-            'message' => 'File uploaded successfully',
-            'data' => $storedFile,
+            'message' => 'File uploaded and chunked successfully.',
+            'content_hash' => $contentHash,
+            'uploaded_metadata' => [
+                'original_filename' => $documentOriginalName,
+                'stored_as' => $fileName,
+                'upload_date_brasilia_local_time' => $uploadedAtBrasilia,
+                'uploaded_at_utc' => $uploadedAtUtc,
+                'file_size' => $fileSize,
+                'extension' => $documentOriginalExtension,
+            ],
+            'processing_info' => [
+                'chunk_total_lines' => $totalLines,
+                'total_chunks' => $chunkIndex,
+                'total_lines' => $totalLines,
+                'total_valid_lines' => ($totalLines - $invalidLines),
+                'total_invalid_lines' => $totalInvalidLines,
+                'total_processing_time_ms' => $totalProcessingTimeMs,
+            ],
             'status' => 201,
             'success' => true,
         ], 201);
     }
+
 
     // #TODO move this function to a helper folder
     private function countCsvLines($path)
